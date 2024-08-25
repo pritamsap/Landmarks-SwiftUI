a01436fe25c4d4480081cde1efe1c07548f6cefc
pritamsap <pritamsap2@gmail.com>
2024-08-25 13:08:33 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by pritam on 2024-08-25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
