6a827eeba7225680c20cd8e1444495ce56c89270
pritamsap <pritamsap2@gmail.com>
2024-08-29 11:49:43 -0400

    Enum Category and list view

---
+++ Landmarks/View/Categories/CategoryHome.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by pritam on 2024-08-29.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
