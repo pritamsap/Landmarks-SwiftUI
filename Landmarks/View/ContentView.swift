5fb5f47784950793e231ae8b73de9e01b400d316
pritamsap <pritamsap2@gmail.com>
2024-08-28 20:17:41 -0400

    Your commit message

--- Landmarks/View/ContentView.swift
+++ Landmarks/View/ContentView.swift
@@ -9,42 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            
-            MapView()
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            
-            VStack(alignment: .leading) {
-                Text("Nepal Landmarks")
-                    .font(.largeTitle)
-                HStack {
-                    Text("Mount Everest")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("Mustang")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                               .foregroundStyle(.secondary)
-                Divider()
-
-
-                               Text("About Mount Everest")
-                                   .font(.title2)
-                Text("The highest mountain in the world.").foregroundStyle(.secondary)
-            }
-            .padding()
-            Spacer()
-        }
-            
+            LandmarkList()
     }
 }
 
 #Preview {
     ContentView()
+        .environment(ModelData())
 }
